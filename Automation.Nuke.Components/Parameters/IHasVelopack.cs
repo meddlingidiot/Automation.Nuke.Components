e8ac85d3c6c3a0400d3289a8280430becfa7a53a
lukelanphear <luke@edgeofcrazy.com>
2025-12-27 21:00:15 -0600

    Update `IHasVelopack` with new Azure Blob storage details and SAS token expiration

--- Automation.Nuke.Components/Parameters/IHasVelopack.cs
+++ Automation.Nuke.Components/Parameters/IHasVelopack.cs
@@ -12,12 +12,14 @@ public interface IHasVelopack: INukeBuild
     [Parameter] string VelopackBlobContainer => TryGetValue(() => VelopackBlobContainer) ?? 
                                                 "installers";
     [Parameter] string AzureBlobAccount => TryGetValue(() => AzureBlobAccount) ?? 
-                                           "staftrinstallers";
+                                           "meddlingidiotinstallers";
+    
+    https://meddlingidiotinstallers.blob.core.windows.net/installers?sp=racwdl&st=2025-12-28T02:30:01Z&se=2029-12-31T10:45:01Z&spr=https&sv=2024-11-04&sr=c&sig=2RTMTJ8fLUFu80HoDRhzEmUargNMveQzK%2BX%2BcdjRF7A%3D
     [Parameter] string AzureBlobEndpoint => TryGetValue(() => AzureBlobEndpoint) ?? 
-                                            "https://staftrinstallers.blob.core.windows.net";
+                                            "https://meddlingidiotinstallers.blob.core.windows.net";
     [Secret] string AzureBlobSasToken => TryGetValue(() => AzureBlobSasToken);
     [Parameter] string AzureBlobSasTokenLocal => TryGetValue(() => AzureBlobSasTokenLocal) ?? 
-                                                 "sp=racwdl&st=2025-11-15T07:03:42Z&se=2125-11-15T15:18:42Z&spr=https&sv=2024-11-04&sr=c&sig=obovzU6WgCnQAMioHdJkG2Pxpq8UP2tqb7CakPAfCU8%3D";
+                                                 "sp=racwdl&st=2025-12-28T02:30:01Z&se=2029-12-31T10:45:01Z&spr=https&sv=2024-11-04&sr=c&sig=2RTMTJ8fLUFu80HoDRhzEmUargNMveQzK%2BX%2BcdjRF7A%3D";
     [Parameter] string AzureBlobTimeout => TryGetValue(() => AzureBlobTimeout) ?? 
                                            "30";
     [Parameter] int KeepMaxReleases => TryGetValue<int?>(() => KeepMaxReleases) ?? 3;
