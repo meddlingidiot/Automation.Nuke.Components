b12eebf385115f27163feb621c4946c79e9254c7
lukelanphear <luke@edgeofcrazy.com>
2026-04-12 19:37:07 -0700

    Remove TUnit test infrastructure and add Apache 2.0 license file

--- nu_get_source_update_commands.sh
+++
@@ -1,2 +0,0 @@
-﻿dotnet nuget update source "AFTR Production" --username llanphear@promiles.com --password <INSERT PAT HERE> --store-password-in-clear-text
-dotnet nuget update source "AFTR Prerelease" --username llanphear@promiles.com --password <INSERT PAT HERE> --store-password-in-clear-text
