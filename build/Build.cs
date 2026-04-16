869837be77553ec1320119a8eda97cc19721377e
lukelanphear <luke@edgeofcrazy.com>
2026-04-15 20:22:55 -0700

    Add BreakBuildOnSecretLeaks property to IHasTests interface

--- build/Build.cs
+++ build/Build.cs
@@ -22,6 +22,7 @@ public class Build : GitHubActionsBuild, IShowVersion, IClean, ICompile, IRestor
         
     string IHasGitHubPackages.GitHubOwner => "meddlingidiot";
     int IHasTests.MinCoverageThreshold => 35;
+    bool IHasTests.BreakBuildOnSecretLeaks => false;
     bool ITestExecution.UseMicrosoftTestingPlatform => true;
     bool IHasTests.UploadToCodecov => true;
     string IHasTests.CodecovToken => "1bcf0725-5a49-4dc4-8d5e-1ad8d4e1aad4";
