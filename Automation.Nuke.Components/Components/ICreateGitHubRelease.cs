be747ebd53d45e4f9fd95ed305df0ce9ee5304a0
lukelanphear <luke@edgeofcrazy.com>
2026-04-17 13:40:09 -0700

    Enhance `ICreateGitHubRelease` to generate release notes using GitHub milestone issues and create comments with release links.

--- Automation.Nuke.Components/Components/ICreateGitHubRelease.cs
+++ Automation.Nuke.Components/Components/ICreateGitHubRelease.cs
@@ -3,6 +3,7 @@ using Nuke.Common;
 using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Git;
 using Nuke.Common.Tools.GitHub;
+using Octokit;
 
 namespace Automation.Nuke.Components.Components;
 
@@ -28,8 +29,34 @@ public interface ICreateGitHubRelease : INukeBuild, IHasGitVersion, IHasGitHubPa
         .OnlyWhenStatic(() => GitHubActions.Instance != null)
         .Executes(async () =>
         {
+            GitHubTasks.GitHubClient = new GitHubClient(new ProductHeaderValue("nuke-build"))
+            {
+                Credentials = new Credentials(GitHubToken)
+            };
+
+            var owner = GitRepository.GetGitHubOwner();
+            var repoName = GitRepository.GetGitHubName();
+
             var issues = await GitRepository.GetGitHubMilestoneIssues(MilestoneTitle);
+
+            var releaseNotes = issues.Count > 0
+                ? "## Issues\n\n" + string.Join("\n", issues.Select(i => $"- #{i.Number} {i.Title}"))
+                : string.Empty;
+
+            var release = await GitHubTasks.GitHubClient.Repository.Release.Create(
+                owner,
+                repoName,
+                new NewRelease(MilestoneTitle)
+                {
+                    Name = MilestoneTitle,
+                    Body = releaseNotes,
+                    Draft = false,
+                    Prerelease = false,
+                });
+
+            Serilog.Log.Information("GitHub release created: {Url}", release.HtmlUrl);
+
             foreach (var issue in issues)
-                await GitHubActions.Instance.CreateComment(issue.Number, $"Released in {MilestoneTitle}! 🎉");
+                await GitHubTasks.GitHubClient.Issue.Comment.Create(owner, repoName, issue.Number, $"Released in [{MilestoneTitle}]({release.HtmlUrl})! 🎉");
         });
 }
